1d818cfa1e27a0c0908c504de4671f2e7125d712
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-12-19 10:28:29 +0900

    an inital project related files are included

---
+++ runTrack/src/main/java/dev/simon/runTrack/RunTrackApplication.java
@@ -0,0 +1,13 @@
+package dev.simon.runTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunTrackApplication.class, args);
+	}
+
+}
---
+++ runTrack/src/test/java/dev/simon/runTrack/RunTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simon.runTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
